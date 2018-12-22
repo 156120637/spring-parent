bb615edbe7b53c5576e52563fc9ab94a822f88c2
tianjiaqin <156120637@qq.com>
2018-12-22 11:24:42 +0800

    del feature elasticsearch

--- spring-repository/spring-repository-elasticsearch/src/main/java/com/dangdang/repository/elasticsearch/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dangdang.repository.elasticsearch.repository;
-
-/**
- * Create by tianjiaqin 2018-12-18
- */
-//public interface UserRepository extends ElasticsearchRepository<UserModel, Long> {
-//
-//}
