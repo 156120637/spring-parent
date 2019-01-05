687c75017f874c50c54f4b92d29d2c74e88a5469
tianjiaqin <156120637@qq.com>
2019-01-05 11:42:38 +0800

    init bussiness model

---
+++ spring-service/spring-service-listener/spring-service-listener-base/src/main/java/com/dangdang/service/listener/base/SpringServiceListenerBaseApplication.java
@@ -0,0 +1,14 @@
+package com.dangdang.service.listener.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServiceListenerBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServiceListenerBaseApplication.class, args);
+    }
+
+}
+
