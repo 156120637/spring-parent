4271453b6bb7910917b54796941705fdfad9a296
tianjiaqin <156120637@qq.com>
2019-03-19 23:18:12 +0800

    feature elastic-job some doc

--- spring-service/spring-service-job/spring-service-job-base/src/main/java/com/dangdang/service/job/base/service/BaseElasticJob.java
+++ spring-service/spring-service-job/spring-service-job-base/src/main/java/com/dangdang/service/job/base/service/BaseElasticJob.java
@@ -20,6 +20,13 @@ public class BaseElasticJob implements SimpleJob {
     @Override
     public void execute(ShardingContext shardingContext) {
 
+        /**
+         * 其实elastic-job最大的好处是可以分片，多机器部署，我觉得这是elastic-job比其他定时作业强的很大一点
+         * shardingContext.getShardingItem()  获取每个机器的分片信息  总片信息  shardingTotalCount配置的
+         * shardingItemParameters 分片的名称
+         *
+         */
+
         log.info("job has start 2 second  ,please check.....begin your bussiness code");
 
 
