0ecffaaf92781bda584c7d43c4ee7d5a684e2477
tianjiaqin <156120637@qq.com>
2019-04-13 15:55:15 +0800

    fix repository hive

--- spring-repository/spring-repository-hive/src/main/java/com/dangdang/repository/hive/mapper/HiveModelMapper.java
+++ spring-repository/spring-repository-hive/src/main/java/com/dangdang/repository/hive/mapper/HiveModelMapper.java
@@ -1,6 +1,7 @@
 package com.dangdang.repository.hive.mapper;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class HiveModelMapper {
 
+    @Autowired
     private JdbcTemplate hiveJdbcTemplate;
 
     public Boolean insertHiveTable(String sql) {
